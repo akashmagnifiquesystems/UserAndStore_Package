b0719ace8725d87d148c29f30d15edecc77b099a
Mobile App Developer <aakashpatel.iosdev@gmail.com>
2021-11-16 19:54:28 +0530

    Cascade App Update : 16 Nov 2021
    -- Worked on User Module package:
    	1.	 Database model -> Done
    	2.	 Project model -> Done
    
    -- Worked on Store Module package:
    	1.	 Database model -> Done
    	2.	 Project model -> Done

--- Package.swift
+++ Package.swift
@@ -27,6 +27,7 @@ let package = Package(
         .package(url: "https://github.com/krimpedance/KRProgressHUD.git", from: "3.4.7")
     ],
     targets: [
+        
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
